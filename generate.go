f168c0bcdddbefca0281ad6e0b25d01fe7b81d80
Nicolás Parada <parada.nicolas@outlook.com>
2026-02-23 00:46:25 -0300

    remove service mock gen cmd

--- generate.go
+++ generate.go
@@ -1,4 +1,3 @@
 package nakama
 
 //go:generate go tool moq -rm -stub -out ./mailing/sender_mock.go ./mailing Sender
-//go:generate go tool moq -rm -stub -out ./transport/service_mock.go ./transport Service
